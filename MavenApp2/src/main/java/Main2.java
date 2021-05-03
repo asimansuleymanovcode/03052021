88729f1967d86f7ec43f9151c7ceb8a5256cb502
[Asadullah Jamalov] <[asadullahnj@code.edu.az]>
2021-05-03 23:51:40 +0400

    my commit

---
+++ MavenApp2/src/main/java/Main2.java
@@ -0,0 +1,11 @@
+import com.sun.tools.javac.Main;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Main Iki calishdi");
+    }
+
+    public static String testMain2(){
+        return "Test";
+    }
+}
---
+++ MyMavenProject/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Main2.testMain2());
+    }
+
+    public static String test(){
+        return "Test";
+    }
+}
