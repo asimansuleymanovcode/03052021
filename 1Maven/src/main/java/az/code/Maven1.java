7325edecc3aee5785df9a99c0bb57cfbd66df64d
= <nurlanam@code.edu.az>
2021-05-03 20:27:38 +0400

    add-maven-ptojects

---
+++ 1Maven/src/main/java/az/code/Maven1.java
@@ -0,0 +1,11 @@
+package az.code;
+
+public class Maven1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static String StringTest(){
+        return "Test";
+    }
+}
---
+++ 2Maven/src/main/java/az/code/Maven2.java
@@ -0,0 +1,7 @@
+package az.code;
+
+public class Maven2 {
+    public static void main(String[] args) {
+        System.out.println(Maven1.StringTest());
+    }
+}
